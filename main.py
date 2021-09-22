6a27cb67259b1f7e8fcc62127f9785cb520e6281
Stephen English <stephanos.english@gmail.com>
2021-09-22 10:09:07 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 33.150673
+MY_LONG = -96.823608
+
+# use latlong.net to find your latitude and longitude location
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+
+print(time_now.hour)
